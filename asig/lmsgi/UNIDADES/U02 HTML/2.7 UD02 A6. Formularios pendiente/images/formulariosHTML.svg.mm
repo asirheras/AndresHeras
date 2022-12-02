ebcf680c6b5c966579226fe141d1dc9bd8c99a03
Andres <q92helea@hotmail.com>
2022-12-02 05:54:11 +0100

    modificada imagen

---
+++ asig/lmsgi/UNIDADES/U02 HTML/2.7 UD02 A6. Formularios pendiente/images/formulariosHTML.svg.mm
@@ -0,0 +1,629 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1669535387042" ID="ID_775886371" MODIFIED="1669637813175">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="text-align: center">
+      FORMULARIOS
+    </p>
+    <p style="text-align: center">
+      contenedor de controles
+    </p>
+    <p style="text-align: center">
+      <a href="https://developer.mozilla.org/es/docs/Web/HTML/Element/form">&lt;form&gt;</a>
+    </p>
+  </body>
+</html></richcontent>
+<edge STYLE="linear"/>
+<font NAME="Consolas" SIZE="28"/>
+<node CREATED="1669538282966" ID="ID_1922964510" MODIFIED="1669624272042" POSITION="right" TEXT="">
+<node CREATED="1669536524557" HGAP="38" ID="ID_488648549" MODIFIED="1669624514434" STYLE="bubble" VSHIFT="43">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <dl>
+      <dt>
+        <b>CONTROLES</b>
+      </dt>
+      <dd>
+        Donde se insertan los datos
+      </dd>
+    </dl>
+  </body>
+</html></richcontent>
+<font NAME="Consolas" SIZE="28"/>
+<icon BUILTIN="full-1"/>
+<node CREATED="1669538203858" HGAP="28" ID="ID_1858284914" MODIFIED="1669551412454" VSHIFT="101">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="text-align: center">
+      <a href="https://developer.mozilla.org/es/docs/Web/HTML/Element/fieldset">&lt;fieldset&gt;</a>
+    </p>
+    <p style="text-align: center">
+      <i>agrupar</i>
+    </p>
+  </body>
+</html></richcontent>
+<font NAME="Consolas" SIZE="28"/>
+<node CREATED="1669538286674" ID="ID_26568607" MODIFIED="1669636201701">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="text-align: center">
+      <font size="5">&lt;legend&gt;</font>
+    </p>
+    <p style="text-align: center">
+      T&#205;TULO de &lt;fieldset&gt;
+    </p>
+  </body>
+</html></richcontent>
+<edge WIDTH="8"/>
+<font NAME="Consolas" SIZE="20"/>
+<node CREATED="1669538199646" HGAP="25" ID="ID_1681760441" MODIFIED="1669636185653" TEXT="&lt;label&gt;" VSHIFT="-66">
+<edge WIDTH="8"/>
+<font NAME="Consolas" SIZE="20"/>
+<node CREATED="1669537880188" ID="ID_902148753" MODIFIED="1669626810809" TEXT="ELEMENTOS">
+<font NAME="Consolas" SIZE="20"/>
+<node CREATED="1669537601164" ID="ID_1645628649" MODIFIED="1669537824759" TEXT="&lt;input&gt;">
+<font NAME="Consolas" SIZE="24"/>
+<node CREATED="1669549080663" ID="ID_1585331436" MODIFIED="1669550388150" TEXT="ATRIBUTOS">
+<node CREATED="1669550391371" HGAP="9" ID="ID_584730612" MODIFIED="1669956751775" TEXT="COMUNES" VSHIFT="-33">
+<font NAME="Consolas" SIZE="16"/>
+<node CREATED="1669549133726" ID="ID_978229768" MODIFIED="1669626872341" TEXT="name=&#x201d;variable&#x201d; id=&#x201d;variable&#x201d;">
+<font NAME="Consolas" SIZE="16"/>
+</node>
+<node CREATED="1669549135618" ID="ID_744387699" MODIFIED="1669626872347" TEXT="value=&#x201c;valor num&#xe9;rico/texto&#x201d;">
+<font NAME="Consolas" SIZE="16"/>
+</node>
+<node CREATED="1669549136180" ID="ID_1668701844" MODIFIED="1669626872347" TEXT="size=&#x201c;valor num&#xe9;rico&#x201d;">
+<font NAME="Consolas" SIZE="16"/>
+</node>
+<node CREATED="1669549136699" ID="ID_1443149345" MODIFIED="1669626872346" TEXT="maxlength=&#x201c;valor num&#xe9;rico&#x201d;">
+<font NAME="Consolas" SIZE="16"/>
+</node>
+<node CREATED="1669549137188" ID="ID_151794641" MODIFIED="1669626872346" TEXT="checked ">
+<font NAME="Consolas" SIZE="16"/>
+</node>
+<node CREATED="1669549137650" ID="ID_609863619" MODIFIED="1669626872345" TEXT="pattern=&#x201d;expresion_regular&#x201d;">
+<font NAME="Consolas" SIZE="16"/>
+</node>
+<node CREATED="1669549138095" ID="ID_233496754" MODIFIED="1669626872345" TEXT="multiple">
+<font NAME="Consolas" SIZE="16"/>
+</node>
+<node CREATED="1669549138562" ID="ID_1936616613" MODIFIED="1669626872345" TEXT="min=&#x201d;no&#x201d;">
+<font NAME="Consolas" SIZE="16"/>
+</node>
+<node CREATED="1669549139048" ID="ID_1732958808" MODIFIED="1669626872345" TEXT="max=&#x201d;no&#x201d; ">
+<font NAME="Consolas" SIZE="16"/>
+</node>
+<node CREATED="1669549139489" ID="ID_1843675199" MODIFIED="1669626872344" TEXT="placeholder =&#x201d;texto&#x201d;">
+<font NAME="Consolas" SIZE="16"/>
+</node>
+</node>
+<node CREATED="1669550401205" ID="ID_1919820316" MODIFIED="1669626906372" TEXT="type">
+<font NAME="Consolas" SIZE="16"/>
+<node CREATED="1669550526395" ID="ID_840965876" MODIFIED="1669626880323" TEXT="=&quot;text&quot; CUADRO DE TEXTO">
+<font NAME="Consolas" SIZE="16"/>
+</node>
+<node CREATED="1669550531609" ID="ID_1622377179" MODIFIED="1669626880330" TEXT="=&#x201d;tel&#x201d; TELEFONO">
+<font NAME="Consolas" SIZE="16"/>
+<node CREATED="1669550774443" ID="ID_1882969978" MODIFIED="1669550779739" TEXT="pattern"/>
+</node>
+<node CREATED="1669550532198" ID="ID_1278728898" MODIFIED="1669626880330" TEXT="=&quot;url&quot; URL">
+<font NAME="Consolas" SIZE="16"/>
+</node>
+<node CREATED="1669550532761" ID="ID_1031150176" MODIFIED="1669626880330" TEXT="=&quot;email&quot; EMAIL">
+<font NAME="Consolas" SIZE="16"/>
+</node>
+<node CREATED="1669550533619" ID="ID_1014460647" MODIFIED="1669626880330" TEXT="=&quot;password&quot; CUADRO DE CONTRASE&#xd1;A">
+<font NAME="Consolas" SIZE="16"/>
+</node>
+<node CREATED="1669550534682" ID="ID_1832137068" MODIFIED="1669626880329" TEXT=" =&quot;date&quot; Fecha y Hora">
+<font NAME="Consolas" SIZE="16"/>
+</node>
+<node CREATED="1669550535394" ID="ID_1730916617" MODIFIED="1669626880329" TEXT="=&quot;number&quot; NUMERO">
+<font NAME="Consolas" SIZE="16"/>
+</node>
+<node CREATED="1669550536016" ID="ID_969987050" MODIFIED="1669626880329" TEXT="=&quot;range&quot; RANGO">
+<font NAME="Consolas" SIZE="16"/>
+</node>
+<node CREATED="1669550536617" ID="ID_595762041" MODIFIED="1669626880329" TEXT="=&quot;color&quot; COLOR">
+<font NAME="Consolas" SIZE="16"/>
+</node>
+<node CREATED="1669550537224" ID="ID_325611508" MODIFIED="1669626880328" TEXT="=&quot;checkbox&quot; CASILLAS DE VERIFICACI&#xd3;N">
+<font NAME="Consolas" SIZE="16"/>
+</node>
+<node CREATED="1669550537782" ID="ID_1224839801" MODIFIED="1669626880328" TEXT="=&quot;radio&quot; BOTONES RADIO">
+<font NAME="Consolas" SIZE="16"/>
+</node>
+<node CREATED="1669550538390" ID="ID_275926167" MODIFIED="1669626880327" TEXT="=&quot;file&quot; ">
+<font NAME="Consolas" SIZE="16"/>
+</node>
+<node CREATED="1669578895469" ID="ID_1784895981" MODIFIED="1669626880326" TEXT="=&quot;submit&quot; ENVIO DE FORMULARIO">
+<font NAME="Consolas" SIZE="16"/>
+</node>
+<node CREATED="1669579419600" ID="ID_355946182" MODIFIED="1669626880325" TEXT="=&quot;reset&quot; REINICIAR">
+<font NAME="Consolas" SIZE="16"/>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1669537744101" ID="ID_1551210625" MODIFIED="1669785907583" TEXT="&lt;textarea&gt;">
+<font NAME="Consolas" SIZE="24"/>
+<node CREATED="1669576560557" ID="ID_1124440459" MODIFIED="1669626906372" TEXT="rows">
+<font NAME="Consolas" SIZE="16"/>
+</node>
+<node CREATED="1669576563435" ID="ID_195504172" MODIFIED="1669626906372" TEXT="cols">
+<font NAME="Consolas" SIZE="16"/>
+</node>
+<node CREATED="1669785912417" ID="ID_622898388" MODIFIED="1669785918964" TEXT="WRAPS"/>
+</node>
+<node CREATED="1669537743491" ID="ID_392895636" MODIFIED="1669578216922">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="text-align: center">
+      <font size="6">&lt;select&gt;</font>
+    </p>
+    <p style="text-align: center">
+      <font size="4">lista desplegable</font>
+    </p>
+  </body>
+</html></richcontent>
+<font NAME="Consolas" SIZE="24"/>
+<node CREATED="1669577315141" ID="ID_783414601" MODIFIED="1669626906372" TEXT="ELEMENTOS">
+<font NAME="Consolas" SIZE="16"/>
+<node CREATED="1669576966195" ID="ID_577616557" MODIFIED="1669626928123" TEXT="optgroup">
+<font NAME="Consolas" SIZE="16"/>
+</node>
+<node CREATED="1669577127091" ID="ID_1230887262" MODIFIED="1669626928122" TEXT="option (OBLIGATORIO)">
+<font NAME="Consolas" SIZE="16"/>
+<node CREATED="1669578433252" ID="ID_944996037" MODIFIED="1669626928121" TEXT="atributos">
+<font NAME="Consolas" SIZE="16"/>
+<node CREATED="1669578441464" ID="ID_218578467" MODIFIED="1669626928121" TEXT="selected">
+<font NAME="Consolas" SIZE="16"/>
+</node>
+<node CREATED="1669578443608" ID="ID_633873528" MODIFIED="1669626928121" TEXT="value">
+<font NAME="Consolas" SIZE="16"/>
+</node>
+<node CREATED="1669578772106" ID="ID_1171413488" MODIFIED="1669626928121" TEXT="label">
+<font NAME="Consolas" SIZE="16"/>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1669578206282" ID="ID_1488052115" MODIFIED="1669626906372" TEXT="ATRIBUTOS">
+<font NAME="Consolas" SIZE="16"/>
+<node CREATED="1669578236768" ID="ID_743138766" MODIFIED="1669626928122" TEXT="multiple">
+<font NAME="Consolas" SIZE="16"/>
+</node>
+<node CREATED="1669578238515" ID="ID_657910139" MODIFIED="1669626928122" TEXT="size">
+<font NAME="Consolas" SIZE="16"/>
+</node>
+</node>
+</node>
+<node CREATED="1669537744693" ID="ID_838437932" MODIFIED="1669580055666">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="text-align: center">
+      &lt;button&gt;
+    </p>
+    <p style="text-align: center">
+      - vs &lt;input&gt;
+    </p>
+  </body>
+</html></richcontent>
+<font NAME="Consolas" SIZE="24"/>
+<node CREATED="1669580096056" ID="ID_810146181" MODIFIED="1669626906372" TEXT="ATRIBUTOS">
+<font NAME="Consolas" SIZE="16"/>
+<node CREATED="1669585281424" ID="ID_117632047" MODIFIED="1669626928122" TEXT="value= &#x201c;valor&#x201d;">
+<font NAME="Consolas" SIZE="16"/>
+</node>
+<node CREATED="1669585287382" ID="ID_1104991664" MODIFIED="1669626928121" TEXT="type = &quot;submit&quot;|&quot;button&quot;|&quot;reset&quot;">
+<font NAME="Consolas" SIZE="16"/>
+</node>
+</node>
+</node>
+<node CREATED="1669539567604" HGAP="18" ID="ID_1515984434" MODIFIED="1669956754832" VSHIFT="35">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="text-align: center">
+      ATRIBUTOS
+    </p>
+    <p style="text-align: center">
+      PRINCIPALES
+    </p>
+  </body>
+</html></richcontent>
+<edge STYLE="linear" WIDTH="8"/>
+<font NAME="Consolas" SIZE="24"/>
+<node CREATED="1669540992726" ID="ID_56693783" MODIFIED="1669626906369">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="text-align: center">
+      name=&#8221;variable&#8221;
+    </p>
+    <p style="text-align: center">
+      id=&#8221;variable&#8221;
+    </p>
+  </body>
+</html></richcontent>
+<font NAME="Consolas" SIZE="16"/>
+</node>
+<node CREATED="1669540994446" ID="ID_1456854842" MODIFIED="1669626906372" TEXT="disabled">
+<font NAME="Consolas" SIZE="16"/>
+</node>
+<node CREATED="1669540995322" ID="ID_1357054313" MODIFIED="1669626906372" TEXT="readonly">
+<font NAME="Consolas" SIZE="16"/>
+</node>
+<node CREATED="1669540996121" ID="ID_229615580" MODIFIED="1669626906371" TEXT="required">
+<font NAME="Consolas" SIZE="16"/>
+</node>
+<node CREATED="1669540997004" ID="ID_751268027" MODIFIED="1669626906371" TEXT="autofocus">
+<font NAME="Consolas" SIZE="16"/>
+</node>
+<node CREATED="1669548802821" ID="ID_43823485" MODIFIED="1669626906371">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="text-align: center">
+      acceskey
+    </p>
+    <p style="text-align: center">
+      =&quot;caracter&quot;
+    </p>
+  </body>
+</html></richcontent>
+<font NAME="Consolas" SIZE="16"/>
+</node>
+<node CREATED="1669548819007" ID="ID_270665258" MODIFIED="1669626906370">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="text-align: center">
+      tabindex
+    </p>
+    <p style="text-align: center">
+      =&quot;n&#186;&quot;
+    </p>
+  </body>
+</html></richcontent>
+<font NAME="Consolas" SIZE="16"/>
+</node>
+</node>
+</node>
+<node CREATED="1669549033141" HGAP="32" ID="ID_70107608" MODIFIED="1669786048315" VSHIFT="25">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="text-align: center">
+      <b>atributo FOR</b>
+    </p>
+    <p style="text-align: center">
+      Vincula label con su respectivo control: Al poner el puntero sobre el label, selecciona el control
+    </p>
+    <p style="text-align: center">
+      
+    </p>
+  </body>
+</html></richcontent>
+<edge STYLE="sharp_bezier"/>
+<font NAME="Consolas" SIZE="20"/>
+</node>
+</node>
+<node CREATED="1669539845430" HGAP="25" ID="ID_1638329145" MODIFIED="1669636264065" TEXT="ATRIBUTO align " VSHIFT="43">
+<edge STYLE="linear" WIDTH="2"/>
+<font NAME="Consolas" SIZE="20"/>
+</node>
+</node>
+</node>
+<node CREATED="1669551419388" HGAP="61" ID="ID_1062757246" MODIFIED="1669636552368" TEXT="ATRIBUTOS" VSHIFT="-200">
+<edge STYLE="linear"/>
+<font NAME="Consolas" SIZE="16"/>
+</node>
+</node>
+</node>
+<node CREATED="1669540797096" ID="ID_1469264775" MODIFIED="1669624342059" POSITION="left" TEXT="SINTAXIS">
+<font NAME="Consolas" SIZE="24"/>
+<icon BUILTIN="full-2"/>
+<node CREATED="1669536796897" ID="ID_1838525081" MODIFIED="1669540851216">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <img src="formulariosHTML_177973795661976800.jpeg" />
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1669539931830" ID="ID_1054723577" MODIFIED="1669637843599" POSITION="left">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font size="6">ATRIBUTOS</font>
+    </p>
+  </body>
+</html></richcontent>
+<icon BUILTIN="full-3"/>
+<node CREATED="1669539959579" ID="ID_1137627616" MODIFIED="1669540266480">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="text-align: center">
+      <font size="6">action</font>
+    </p>
+    <p style="text-align: center">
+      =URL
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1669539960460" ID="ID_1649307386" MODIFIED="1669540100048">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="text-align: center">
+      <font size="6">method:</font>
+    </p>
+    <p style="text-align: center">
+      POST|GET
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1669540053006" ID="ID_752496169" MODIFIED="1669540083531">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font size="6">enctype</font>
+    </p>
+    <p>
+      TIPO DE CONTENIDO
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1669540126821" ID="ID_1855022523" MODIFIED="1669540169104">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="text-align: center">
+      <font size="6">accept-charset</font>
+    </p>
+    <p style="text-align: center">
+      lista_codificaci&#243;n
+    </p>
+    <p style="text-align: center">
+      de caracteres
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1669539969859" ID="ID_930501225" MODIFIED="1669540233624">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="text-align: center">
+      <font size="6">accept</font>
+    </p>
+    <p style="text-align: center">
+      Lista de
+    </p>
+    <p style="text-align: center">
+      tipos de
+    </p>
+    <p style="text-align: center">
+      contenidos
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1669540183249" ID="ID_1188856414" MODIFIED="1669540277167" TEXT="id">
+<font NAME="Consolas" SIZE="24"/>
+</node>
+<node CREATED="1669789311892" ID="ID_7349347" MODIFIED="1669789345787">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font size="6">target</font>
+    </p>
+    <p>
+      destino
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1669611299087" ID="ID_1174340831" MODIFIED="1669636552369" POSITION="right" STYLE="bubble" TEXT="FORMATO">
+<edge STYLE="linear"/>
+<arrowlink DESTINATION="ID_1174340831" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_51432473" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<linktarget COLOR="#b0b0b0" DESTINATION="ID_1174340831" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_51432473" SOURCE="ID_1174340831" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<font NAME="Consolas" SIZE="28"/>
+<node CREATED="1669611319165" ID="ID_1649476596" MODIFIED="1669636552369" STYLE="bubble" TEXT="ESPACIO CAMPOS ">
+<edge STYLE="linear" WIDTH="2"/>
+<font NAME="Consolas" SIZE="20"/>
+</node>
+<node CREATED="1669611320787" ID="ID_350752209" MODIFIED="1669636552369" STYLE="bubble" TEXT="Etiquetas alineadas y formateadas">
+<edge STYLE="linear"/>
+<font NAME="Consolas" SIZE="20"/>
+<node CREATED="1669611516426" HGAP="32" ID="ID_349078636" MODIFIED="1669636552369" STYLE="bubble" VSHIFT="-4">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      label {
+    </p>
+    <p>
+      display: block;
+    </p>
+    <p>
+      margin: .5em 0 0 0;
+    </p>
+    <p>
+      }
+    </p>
+  </body>
+</html></richcontent>
+<edge COLOR="#808080" STYLE="linear" WIDTH="thin"/>
+<font NAME="Consolas" SIZE="16"/>
+</node>
+<node CREATED="1669611517434" HGAP="34" ID="ID_1062846836" MODIFIED="1669636552369">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      .btn {
+    </p>
+    <p>
+      display: block;
+    </p>
+    <p>
+      margin: 1em 0;
+    </p>
+  </body>
+</html></richcontent>
+<edge STYLE="linear"/>
+<font NAME="Consolas" SIZE="16"/>
+</node>
+</node>
+<node CREATED="1669611321433" ID="ID_1440723362" MODIFIED="1669636552369" STYLE="bubble">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="text-align: center">
+      Formulario en varias columnas
+    </p>
+    <p style="text-align: center">
+      &#243;
+    </p>
+    <p style="text-align: center">
+      CSS al fieldset
+    </p>
+  </body>
+</html></richcontent>
+<edge STYLE="linear"/>
+<font NAME="Consolas" SIZE="20"/>
+<node CREATED="1669611913701" HGAP="60" ID="ID_1531267618" MODIFIED="1669636552369" STYLE="bubble" VSHIFT="19">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      form fieldset {
+    </p>
+    <p>
+      float: left;
+    </p>
+    <p>
+      width: 48%;
+    </p>
+    <p>
+      }
+    </p>
+  </body>
+</html></richcontent>
+<edge STYLE="linear"/>
+<font NAME="Consolas" SIZE="16"/>
+</node>
+</node>
+<node CREATED="1669611322005" ID="ID_849250724" MODIFIED="1669636552369">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Resaltar campo seleccionado
+    </p>
+    <p>
+      (pseudoclase :focus)
+    </p>
+  </body>
+</html></richcontent>
+<edge STYLE="linear"/>
+<font NAME="Consolas" SIZE="20"/>
+<node CREATED="1669612138301" HGAP="78" ID="ID_1015459315" MODIFIED="1669636552369" VSHIFT="34">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      input:focus {
+    </p>
+    <p>
+      border: 2px solid #000;
+    </p>
+    <p>
+      background: #F3F3F3;
+    </p>
+    <p>
+      }
+    </p>
+  </body>
+</html></richcontent>
+<edge STYLE="linear"/>
+<font NAME="Consolas" SIZE="16"/>
+</node>
+</node>
+<node CREATED="1669611322551" ID="ID_1086029568" MODIFIED="1669636552369" TEXT="Botones con iconos">
+<edge STYLE="linear"/>
+<font NAME="Consolas" SIZE="20"/>
+</node>
+</node>
+</node>
+</map>
